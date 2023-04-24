281aa92dc262165d97493eafc5aebc6586c1f566
Subhanshu Mohan Gupta <subhanshumg@Subhanshus-MacBook-Pro.local>
2023-04-24 15:06:33 +0530

    updated keyless-js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './scss/index.scss';
+import KeylessWeb3 from './classes/KeylessWeb3';
+import config from './config/config';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// import safleBlockchainController from '@getsafle/blockchain-controller';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const getNetworks = async () => {
+    const networks = await fetch( config.networks ).then(e=>e.json());
+    const allowedIds = [ 1, 3, 137, 80001 ];
+    return Object.values( networks ).reduce( (acc, e) => acc.concat( e ), [] ).filter( e=> allowedIds.indexOf( e.chainId ) != -1 );
+
+    return networks;
+
+}
+
+
+export {
+    KeylessWeb3,
+    getNetworks
+}
